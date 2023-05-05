371f409fd22509c926b1c78cd3f3cfb33908b576
BrennonSullivan <brennsullivan2@gmail.com>
2023-05-04 20:29:30 -0500

    resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,42 @@
-const { Thought } = require('../models');
+// const { Thought } = require('../models');
 
-const resolvers = {
-  Query: {
+// const resolvers = {
+//   Query: {
 
-  },
+//   },
 
-  Mutation: {
+//   Mutation: {
     
-  },
+//   },
+// };
+
+
+const createRecipe = async (parent, args, context) => {
+  const { title, description, instructions, ingredients, imageUrl } = args.input;
+  const { userId } = context;
+
+  // Create a new Recipe document in the database
+  const recipe = await Recipe.create({
+    title,
+    description,
+    instructions,
+    ingredients,
+    imageUrl,
+    author: userId,
+  });
+
+  // Add the new recipe to the User's recipes array
+  const user = await User.findByIdAndUpdate(
+    userId,
+    { $push: { recipes: recipe._id } },
+    { new: true },
+  );
+
+  // Publish the recipeCreated subscription event to any subscribed clients
+  pubsub.publish('RECIPE_CREATED', { recipeCreated: recipe });
+
+  // Return the new Recipe document to the client
+  return recipe;
 };
 
 module.exports = resolvers;
