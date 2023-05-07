05a9558552b752de7fb649cdb187dd518484fb8f
unknown <jshigoodies@gmail.com>
2023-05-07 01:23:36 -0500

    add login page with cool css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <div className='navLeft'>
-        <a href='/'><p>LOGO GOES HERE</p></a>
-      </div>
-      <div className='navRight'>
-        <Link to='/search'>
-          <button>Search</button>
-        </Link>
-        <Link to='/signup'>
-          <button>SignUp</button>
-        </Link>
-        <Link to='/login'>
-          <button>Login</button>
-        </Link>
-      </div>
-    </nav>
+    <div className='navDiv'>
+        <nav>
+        <div className='navLeft'>
+            <a href='/'><p>LOGO GOES HERE</p></a>
+        </div>
+        <div className='navRight'>
+            <Link to='/search'>
+            <button>Search</button>
+            </Link>
+            <Link to='/signup'>
+            <button>SignUp</button>
+            </Link>
+            <Link to='/login'>
+            <button>Login</button>
+            </Link>
+        </div>
+        </nav>
+    </div>
   );
 }
 
