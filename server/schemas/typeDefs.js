47b8531ac7bffc492dc3e8f242d50ae03a8da63a
Jason Shi <jshigoodies@gmail.com>
2023-05-09 00:13:43 -0500

    fixed database gql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,89 +2,25 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    id: ID!
-    email: String!
-    username: String!
-    recipes: [Recipe!]!
+    _id: ID
+    email: String
+    password: String
+    username: String
   }
 
-  type Recipe {
-    id: ID!
-    title: String!
-    image: String!
-    servings: Int!
-    sourceUrl: String!
-    ingredients: [String!]!
-    author: User!
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  input RecipeInput {
-    title: String!
-    image: String!
-    servings: Int!
-    sourceUrl: String!
-    ingredients: [String!]!
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    currentUser: User
-    recipe(id: ID!): Recipe
-    recipes: [Recipe!]!
-    user(id: ID!): User
-    users: [User!]!
+    users: [User]
+    user(username: String!): User
   }
 
   type Mutation {
-    addRecipe(input: RecipeInput!): Recipe!
-    updateRecipe(id: ID!, input: RecipeInput!): Recipe!
-    deleteRecipe(id: ID!): Recipe!
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
 module.exports = typeDefs;
-// const typeDefs = gql`
-
-// input RecipeInput {
-//   title: String!
-//   description: String!
-//   instructions: String!
-//   ingredients: [String!]!
-//   imageUrl: String
-// }
-
-
-// type User {
-//   id: ID!
-//   name: String!
-//   email: String!
-//   password: String!
-//   recipes: [Recipe!]!
-//   createdAt: String!
-//   updatedAt: String!
-// }
-
-// type Query {
-//   recipes: [Recipe!]!
-//   recipe(id: ID!): Recipe
-//   user(id: ID!): User
-// }
-
-// type Mutation {
-//   createRecipe(input: RecipeInput!): Recipe!
-//   updateRecipe(id: ID!, input: RecipeInput): Recipe!
-//   deleteRecipe(id: ID!): ID
-//   signup(name: String!, email: String!, password: String!): String!
-//   login(email: String!, password: String!): String!
-// }
-
-// type Subscription {
-//   recipeCreated: Recipe!
-//   recipeUpdated: Recipe!
-//   recipeDeleted: ID!
-// }
-
-// `;
-
-// module.exports = typeDefs;
